6a39d364d30f4af9fa9bbbf57cc82360eb03e5ca
Akane <soranyuxe@gmail.com>
2020-01-09 10:12:05 +0100

    Déplacement et mise des scènes

---
+++ Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    public Rigidbody Bombe;
+    public Transform origine;
+    public int nbBombes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Déplacement du personnage
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.Translate(0.1f, 0, 0);
+
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Translate(0, 0, -0.1f);
+        }
+ 
+
+
+            if (Input.GetKey(KeyCode.T) && GameObject.Find("Bombe(Clone)") == null)
+        {
+            Rigidbody instance;
+          instance = Instantiate(Bombe, origine.position, origine.rotation) as Rigidbody;
+        }
+    }
+}
